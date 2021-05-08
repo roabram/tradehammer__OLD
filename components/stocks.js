53ce3a4fdf29960aca1b466d5c515355a621d014
rabram1 <romanabram@web.de>
2021-05-08 17:12:17 +0200

    fixing date string size

--- components/stocks.js
+++ components/stocks.js
@@ -1,14 +1,16 @@
 import { createElement } from '../utils/element';
 
-export function createStockElement({ symbol, open, low, close, volume }) {
+export function createStockElement({ symbol, open, low, close, volume, date }) {
+  date = date.substring(0, 10);
   return createElement('div', {
     className: 'stocks-card',
     children: [
-      createElement('h2', { innerText: symbol }),
-      createElement('p', { innerText: open }),
-      createElement('p', { innerText: close }),
-      createElement('p', { innerText: low }),
-      createElement('p', { innerText: volume }),
+      createElement('h2', { innerText: 'symbol: ' + symbol }),
+      createElement('p', { innerText: 'Open: ' + open }),
+      createElement('p', { innerText: 'Close: ' + close }),
+      createElement('p', { innerText: 'Low: ' + low }),
+      createElement('p', { innerText: 'Volume: ' + volume }),
+      createElement('p', { innerText: 'Date: ' + date }),
     ],
   });
 }
